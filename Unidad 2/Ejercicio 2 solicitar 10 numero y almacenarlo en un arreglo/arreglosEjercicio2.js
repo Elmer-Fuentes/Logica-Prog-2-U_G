f768ee75442b519e089100ef1deca0005670ea88
Elmer-Fuentes <delacruzelmer27@gmail.com>
2025-06-13 09:25:13 -0600

    ejercicio 2 un array que almacene 10 digitos e imprima los numeros digitados

--- Unidad 2/Ejercicio 2 solicitar 10 numero y almacenarlo en un arreglo/arreglosEjercicio2.js
+++ Unidad 2/Ejercicio 2 solicitar 10 numero y almacenarlo en un arreglo/arreglosEjercicio2.js
@@ -1,1 +1,13 @@
-alert(Ejercicio2);
+let numero = []
+for(let i=0; i<10; i++)
+{
+let digitado = parseInt(prompt(`Escribe 10 numeros  ${i + 1}`));
+if (isNaN(digitado)) {
+    
+    alert('No es un digito valido, repite')
+    break;
+}
+else{
+    numero.push(digitado)} // en el array numero con el operador punto y push pasamos el parametro o la variable en si que almacena los valores desde el prompt
+}
+alert(`Los Numero Impresos son ${numero} y se tiene ${numero.length} digitos en el arreglo`);
